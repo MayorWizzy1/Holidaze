14d304f8581c0c942d39f5e2192763d90a733b7d
MayorWizzy <gowonwisdom1@gmail.com>
2025-05-29 13:54:26 +0100

    Find your next stay here!

--- src/components/homePage/HeroSection.jsx
+++ src/components/homePage/HeroSection.jsx
@@ -5,7 +5,7 @@ export function HeroSection({ setSearchResults, setSearchText }) {
     <div className="bg-[url('/public/bg-hero.jpg')] bg-cover bg-center font-roboto">
       <div className="bg-bg-black px-10 py-12 sm:px-24 lg:py-24 lg:px-32">
         <h1 className="font-boska text-white text-[32px] mb-2 text-center font-bold lg:text-[40px] lg:text-left">
-          Find your next stay
+          Find your next stay here!
         </h1>
         <SearchForm
           setSearchResults={setSearchResults}

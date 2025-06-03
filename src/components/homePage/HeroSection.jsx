24c6454796028c5d82fdceb88365be037d934647
MayorWizzy <gowonwisdom1@gmail.com>
2025-06-03 09:53:01 +0100

    Added a line

--- src/components/homePage/HeroSection.jsx
+++ src/components/homePage/HeroSection.jsx
@@ -4,8 +4,8 @@ export function HeroSection({ setSearchResults, setSearchText }) {
   return (
     <div className="bg-[url('/public/bg-hero.jpg')] bg-cover bg-center font-roboto">
       <div className="bg-bg-black px-10 py-12 sm:px-24 lg:py-24 lg:px-32">
-        <h1 className="font-boska text-white text-[32px] mb-2 text-center font-bold lg:text-[40px] lg:text-left">
-          Find your next stay here!
+        <h1 className="font-boska text-white text-[40px] mb-1 text-center font-bold lg:text-[45px] lg:text-left">
+          Find your next stay here! For what? Oga rest!!!!
         </h1>
         <SearchForm
           setSearchResults={setSearchResults}
